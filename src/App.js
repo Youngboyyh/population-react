17c7f15401d7fc3c56882fc153f893005111a94a
Youngboyyh <yh1289718744@gmail.com>
2022-12-24 22:37:36 +0900

    bug_repair

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import CheckBox from './components/CheckBox'
 import PopulationGrowth from './components/Charts'
 
 function App () {
-  const [prefCode, setPreCode] = useState(null)
-  const [prefName, setPrefName] = useState(null)
+  const [prefCode, setPrefCode] = useState(13)
+  const [prefName, setPrefName] = useState("東京都")
   const [random, setRandom] = useState(null)
+  const [dataList, setDataList] = useState([])
+  const [prefList, setPrefList] = useState([13])
+  const [isLoading, setIsLoading] = useState(true)
+
+
   const getMsg = (msg1, msg2, msg3) => {
-    if (msg1 != null) {
-      setPreCode(msg1)
-      setPrefName(msg2)
-      setRandom(msg3)
+    if (msg1 !== null) {
+      if (prefList.includes(msg1)) {
+        const newList = prefList.filter(item => item !== msg1)
+        const newDataList = dataList.filter(item => item.name != msg2)
+        console.log("NEWdatalist", newDataList)
+        setPrefList([...newList])
+        setDataList([...newDataList])
+      } else {
+        const newList = [...prefList, msg1]
+        setPrefCode(msg1)
+        setPrefList([...newList])
+        setRandom(msg3)
+        setPrefName(msg2)
+      }
     }
   }
-
+  //
+  const ischecked = (result) => {
+    var data_copy = []
+    var tem_obj = {}
+    result.forEach((item) => {
+      data_copy.push(item.value)
+    })
+    tem_obj = { name: prefName, data: data_copy }
+    setDataList([...dataList, tem_obj])
+  }
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefCode}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-API-KEY': 'CDmRptvssqaJNGutJw9ARyG8TujoA6q5o5LtwaFe',
+        },
+      })
+      const result = await response.json()
+      // setDataP(result.result.data[0].data)
+      ischecked(result.result.data[0].data)
+      setIsLoading(false)
+    }
+    fetchData()
+  }, [prefCode, random, prefName])
+  //
+  console.log(dataList)
   return (
     <div className="App">
       <h2>都道府県別の総人口推移グラフ</h2>
       <CheckBox getMsg={getMsg} />
-      <PopulationGrowth sendMsg={[prefCode, prefName, random]} />
+      <PopulationGrowth sendMsg={[dataList, isLoading]} />
     </div>
 
   )
--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,59 +1,14 @@
 // import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
 import Highcharts from "highcharts"
 import HighchartsReact from "highcharts-react-official"
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
+
 
 const PopulationGrowth = (props) => {
-  var msg1 = props.sendMsg[0]
-  var msg2 = props.sendMsg[1] || "東京都"
-  var msg3 = props.sendMsg[2]
-  // const [dataP, setDataP] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [prefList, setPrefList] = useState([13])
-  const [prefCode, setPrefCode] = useState(13)
-  const [dataList, setDataList] = useState([])
-  const [prefN, setPrefN] = useState(null)
-  useEffect(() => {
-    if (msg1 !== null) {
-      if (prefList.includes(msg1)) {
-        const newList = prefList.filter(item => item != msg1)
-        const newDataList = dataList.filter(item => item.name != msg2)
-        setPrefList([...newList])
-        setDataList([...newDataList])
-        // setPrefList([...prefList.filter(item => item != msg1)])
-      } else {
-        const newList = [...prefList, msg1]
-        setPrefCode(msg1)
-        setPrefList([...newList])
-        setPrefN(msg3)
-      }
-    }
-  }, [msg3])
-  const ischecked = (result) => {
-    var data_copy = []
-    var tem_obj = {}
-    result.forEach((item, index) => {
-      data_copy.push(item.value)
-    })
-    tem_obj = { name: msg2, data: data_copy }
-    setDataList([...[...dataList, tem_obj]])
-  }
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefCode}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-API-KEY': 'CDmRptvssqaJNGutJw9ARyG8TujoA6q5o5LtwaFe',
-        },
-      })
-      const result = await response.json()
-      // setDataP(result.result.data[0].data)
-      ischecked(result.result.data[0].data)
-      setIsLoading(false)
-    }
-    fetchData()
-  }, [prefCode, prefN])
+  let dataList = props.sendMsg[0]
+  let isLoading = props.sendMsg[1]
+  console.log(dataList, isLoading)
+
   if (isLoading) {
     return (
       <div><h1>Loading...</h1></div>
@@ -104,7 +59,7 @@ const PopulationGrowth = (props) => {
       },
       tickInterval: 5,
     },
-    series: dataList
+    series: dataList,
   }
   return (
     <>
